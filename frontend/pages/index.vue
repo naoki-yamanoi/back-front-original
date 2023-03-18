1f1c5b669c8c18b2b8c1129b8e04e5e06790f602
naoki-yamanoi <ti44nnyy999@gmail.com>
2023-03-18 12:48:56 +0900

    nuxtインストール、エラー解決

---
+++ frontend/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import NuxtLogo from '../components/NuxtLogo.vue'
+
+export default Vue.extend({
+  components: {
+    NuxtLogo,
+  },
+  name: 'IndexPage'
+})
+</script>
